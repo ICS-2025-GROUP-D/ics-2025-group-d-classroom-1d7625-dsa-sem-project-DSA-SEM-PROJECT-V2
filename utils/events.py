633c375f4e6dea64c378c0a1f967cf57c5020738
iLynn94 <ilynn1594@gmail.com>
2025-07-08 13:57:16 +0300

    device_section and events.py added

---
+++ utils/events.py
@@ -0,0 +1,8 @@
+def handle_wifi_switch(val):
+    print(f"WiFi switched: {val}")
+
+def handle_tv_switch(val):
+    print(f"TV switched: {val}")
+
+def handle_thermostat_switch(val):
+    print(f"Thermostat switched: {val}")
